b86f624af876849d2d80fc8231883fd15df30c09
Olena Husliakova <113473257+softly242@users.noreply.github.com>
2023-11-19 17:30:32 +0100

    fix hw

--- src/4.ts
+++ src/4.ts
@@ -1,54 +1,42 @@
 class Key {
-  protected signature: number;
+  protected signature: number = Math.random();
 
-  constructor() {
-    this.signature = Math.random();
-  }
-
-  getSignature() : number {
+  getSignature(): number {
     return this.signature;
   }
 }
 
 class Person {
-  constructor(private key: Key) {
-}
-getKey(): Key {
-  return this.key
-}
+  constructor(private key: Key) {}
+  getKey(): Key {
+    return this.key;
+  }
 }
 
 abstract class House {
-  protected door: boolean;
-  protected key: Key;
+  protected door = false;
   protected tenants: Person[] = [];
 
-  comeIn(person: Person) {
-    if(this.door === true) {
-     this.tenants.push(person)
+  constructor(protected key: Key) {}
+
+  comeIn(person: Person): void {
+    if (this.door) {
+      this.tenants.push(person);
     }
   }
-   abstract openDoor(key:Key): void;
+  abstract openDoor(key: Key): void;
 }
 
-
 class MyHouse extends House {
-  protected door: boolean = false;
-  constructor(protected key: Key) {
-    super();
-  }
   openDoor(key: Key): void {
     if (key.getSignature() === this.key.getSignature()) {
       this.door = true;
-      console.log('Door is opened');
+      console.log("Door is opened");
     } else {
-      console.log('Wrong key. Door is closed');
+      console.log("Wrong key. Door is closed");
     }
   }
 }
-
-
-
 
 const key = new Key();
 
@@ -58,6 +46,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
